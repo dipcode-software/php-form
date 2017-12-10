5277e69e7163490c5b0a1e87d275a4cbdc0fbd24
Sandro Rodrigues <srtabs@gmail.com>
2017-12-10 03:15:30 +0000

    Added docs deploy script

---
+++ docs/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pip install -r docs/requirements.txt
+cd docs/php-form
+mkdocs build
+tar -zcvf phpform.tar.gz -C site .
+curl -X POST -H "Content-Type: multipart/form-data" -H "Api-Key: $ALEXANDRIA_APIKEY" -F "archive=@phpform.tar.gz" https://alexandriadocs.io/api/v1/projects/upload/
