3deaa83ca6fa78aee616ca5f15d6126365a95683
Sandro Rodrigues <srtabs@gmail.com>
2017-12-11 12:42:23 +0000

    Updated deploy script

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
-pip install -r docs/requirements.txt
-cd docs/php-form
-mkdocs build
-tar -zcvf phpform.tar.gz -C site .
-curl -X POST -H "Content-Type: multipart/form-data" -H "Api-Key: ${1}" -F "archive=@phpform.tar.gz" https://alexandriadocs.io/api/v1/projects/upload/
-rm phpform.tar.gz
+#!/bin/bash
+
+cd docs/
+source env/bin/activate && \
+    pip install -r requirements.txt && \
+    cd php-form && \
+    mkdocs build && \
+    tar -zcvf phpform.tar.gz -C site . && \
+    curl -X POST -H "Content-Type: multipart/form-data" -H "Api-Key: ${1}" -F "archive=@phpform.tar.gz" https://alexandriadocs.io/api/v1/projects/upload/ && \
+    rm phpform.tar.gz
