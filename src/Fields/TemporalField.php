498ab0c587cb55d63bece2bb470c3134c1bf2251
Tiago Brito <tfb@eurotux.com>
2018-04-21 19:03:20 +0100

    Fixed validation of dates

--- src/Fields/TemporalField.php
+++ src/Fields/TemporalField.php
@@ -18,6 +18,11 @@ class TemporalField extends Field
         parent::__construct($args);
     }
 
+    public function isValidDate($date_original, $date_created)
+    {
+        return $date_created && $date_created->format($this->format) == '!' . $date_original;
+    }
+
     public function toNative($value)
     {
         if (empty($value)) {
@@ -26,7 +31,7 @@ class TemporalField extends Field
 
         $date = date_create_from_format($this->format, $value);
 
-        if (!$date) {
+        if (!$date || !$this->isValidDate($value, $date)) {
             throw new ValidationError($this->error_messages['invalid'], 'invalid');
         }
 
